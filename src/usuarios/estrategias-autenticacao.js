dd51008ef70139245a8791e02f54f94be92ef43c
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-23 01:04:24 -0300

    Configurando a estratégia local

---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const bcript = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros');
+
+function verificaUsuario(usuario) {
+    if(!usuario) {
+        throw new InvalidArgumentError('Usuário não encontrado!');
+    }
+}
+
+async function verificaSenha(senha, senhaHash) {
+    const senhaValida = await bcrypt.compare(senha, senhaHash);
+
+    if(!senhaValida) {
+        throw new InvalidArgumentError('Senha inválida!');
+    }
+    
+    return ;
+}
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'senha',
+        session: false
+    }, async (email, senha, done) => {
+        try {
+            const usuario = await Usuario.buscaPorEmail(email);
+            verificaUsuario(usuario);
+            verificaSenha(senha, usuario.senhaHash);
+            done(null, usuario);    
+        } catch (error) {
+            done(error);
+        }
+
+    })
+);
