3c4dc14d44b89b70d92b2c7b6416980a519aa047
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-24 16:38:37 -0300

    Tratando erros do token

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 
 module.exports = {
-    local: (req, res, next) =>{
+    local: (req, res, next) => {
             passport.authenticate(
                 'local', 
                 { session: false }, 
@@ -22,5 +22,28 @@ module.exports = {
                     return next();
                 }
         )(req, res, next);
-    }        
+    },
+    
+    bearer: (req, res, next) => {
+        passport.authenticate(
+            'bearer',
+            { session: false },
+            (error, usuario, info) => {
+                if(error && error.name === 'JsonWebTokenError') {
+                    return res.status(401).json({ error: error.message });
+                }
+
+                if(error) {
+                    return res.status(500).json( { error: error.message });
+                }
+
+                if(!usuario) {
+                    return res.status(401).json();
+                }
+
+                req.user = usuario;
+                return next();
+            }
+        )(req, res, next);
+    }
 };
--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,6 +1,5 @@
 const usuariosControlador = require('./usuarios-controlador');
 const middlewaresAutenticacao = require('./middlewares-autenticacao');
-const passport = require('passport');
 
 module.exports = app => {
   app
@@ -16,7 +15,7 @@ module.exports = app => {
     .get(usuariosControlador.lista);
 
   app.route('/usuario/:id').delete(
-    passport.authenticate('bearer', { session: false }),
+    middlewaresAutenticacao.bearer,
     usuariosControlador.deleta
   );
 };
