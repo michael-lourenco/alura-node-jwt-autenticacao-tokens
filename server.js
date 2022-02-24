121e44b6cf97c138ef78ff6d03a1957610d4a59b
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-24 18:29:12 -0300

    Criando uma blacklist

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const port = 3000;
 const db = require('./database');
+require('./redis/blackList');
 
 const routes = require('./rotas');
 routes(app);
